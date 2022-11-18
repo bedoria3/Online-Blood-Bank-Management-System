95763941e6286904054c824bc323f447999676fa
LorenCuadero <marygracevellina@gmail.com>
2022-11-18 10:39:02 +0800

    Userdashboard UI

---
+++ user.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./user.css">
+    <title>Online Blood Bank Management System</title>
+</head>
+<body>
+<div class="header">
+  <img src="./userimages/logofinal.png" alt="logo" style = "width:100px; margin-top: -22px"/>
+  <h1 style = " font-size: 30px; margin-left: 110px;margin-top: -58px">Online Blood Bank Management System</h1>
+</div>
+
+
+<div class="wrapper">
+    <div class="sidebar">
+        <ul>
+            <li><a href="user.php"><img src="./userimages/home.png"/><h4 style = "margin-left: 70px;margin-top:-28px">Home</h4></a></li>
+            <li><a href="Donate.php"><img src="./userimages/donate.png"/><h4 style = "margin-left: 70px;margin-top:-33px">Donate</h4></a></li>
+            <li><a href="sendrequest.php"><img src="./userimages/send (1).png"/><h4 style = "margin-left: 70px;margin-top:-33px">Send Request</h4></a></li>
+            <li><a href="viewrequest.php"><img src="./userimages/view.png"/><h4 style = "margin-left: 70px;margin-top:-32px">View Request</h4></a></li>
+            <li><a href="blooddetails.php"><img src="./userimages/blood.png"/><h4 style = "margin-left: 70px;margin-top:-32px">Blood Details</h4></a></li>
+            <li><a href="camps.php"><img src="./userimages/camps.png"/><h4 style = "margin-left: 70px;margin-top:-32px">Camps</h4></a></li>
+
+        </ul> 
+      
+    </div>
+    <div class="main_content" >
+        <div class="info">
+            <br><br>
+          <div>You Don't Need To</div><br>
+          <div>Be A Doctor To</div><br>
+          <div>Save <span style = "color: red;">Lives</span></div><br>
+          <div>Just Donate <span style = "color: red;">Blood</span></div><br>
+          <div class="info1">It's SAFE. It's SIMPLE. And it saves LIVES</div>
+      </div>
+      <img src="./userimages/blood-removebg-preview.png" class="donate">
+    </div>
+</div>
+</body>
+</html>
