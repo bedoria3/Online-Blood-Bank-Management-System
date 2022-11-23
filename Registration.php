580a8cb7d3593b7e7e4c826d8607a59c4a452e66
soseit <soseitbedoria@gmail.com>
2022-11-23 21:31:19 +0800

    Updated registration

--- Registration.php
+++ Registration.php
@@ -1,61 +1,77 @@
+<?php
+@include 'dbconnect.php';
+
+if(isset($_POST['submit'])){
+
+   $fullname = mysqli_real_escape_string($conn, $_POST['fullname']);
+   $username = mysqli_real_escape_string($conn, $_POST['username']);
+   $email = mysqli_real_escape_string($conn, $_POST['email']);
+   $pass = ($_POST['password']);
+   $cpass = ($_POST['cpassword']);
+
+   $select = " SELECT * FROM registration WHERE email = '$email' && password = '$pass' ";
+
+   $result = mysqli_query($conn, $select);
+
+   if(mysqli_num_rows($result) > 0){
+      $error[] = 'user already exist!';
+   }else{
+
+      if($pass != $cpass){
+         $error[] = 'password not matched!';
+      }else{
+         $insert = "INSERT into `registration` (fullname,username,email_address, password)
+         VALUES ('$fullname','$username', '$email','$pass')";
+         $result = mysqli_query($conn, $insert) or die(mysqli_error($conn));
+         $rows = mysqli_num_rows($result);
+         header('location:user_login.php');
+      }
+   }
+
+};
+
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Online Blood Bank</title>
-    <link rel="stylesheet" href="style.css">
+   <meta charset="UTF-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>Registration Form</title>
+
+   <!-- custom css file link  -->
+   <link rel="stylesheet" href="user.css">
+<style>
+   body{
+      background-image: linear-gradient(to right, red , white);
+   }
+</style>
 </head>
 <body>
-<?php
-    require_once('dbconnect.php');
+   
+<div class="form-container">
 
-    if (isset($_REQUEST['fullname'])) {
+   <form action="" method="post">
+      <h3>Registration Form</h3>
+      <?php
+      if(isset($error)){
+         foreach($error as $error){
+            echo '<span class="error-msg">'.$error.'</span>';
+         };
+      };
+      ?>
+      <input type="text" name="fullname" required placeholder="Full Name">
+      <input type="username" name="username" required placeholder="Username">
+      <input type="email" name="email" required placeholder="Email">
+      <input type="password" name="password" required placeholder="Password">
+      <input type="password" name="cpassword" required placeholder="Re-type Password">
+      <input type="submit" name="submit" value="register now" class="form-btn">
+      <p>Already have an account? <a href="user_login.php">Login now</a></p>
+   </form>
 
-        $full_name = stripslashes($_REQUEST['fullname']);
-        $full_name = mysqli_real_escape_string($conn, $full_name);
+</div>
 
-        $username = stripslashes($_REQUEST['username']);
-        $username = mysqli_real_escape_string($conn, $username);
-
-        $email    = stripslashes($_REQUEST['email']);
-        $email    = mysqli_real_escape_string($conn, $email);
-
-        $password = stripslashes($_REQUEST['password']);
-        $password = mysqli_real_escape_string($conn, $password);
-
-      
-        $query    = "INSERT into `registration` (fullname,username,email_address, password)
-                     VALUES ('$full_name','$username', '$email','$password')";
-        $result   = mysqli_query($conn, $query) or die("Error in adding account: ".mysqli_error($conn));
-
-        if ($result) {
-            echo "<div class='form'>
-                  <h3>You are registered successfully.</h3><br/>
-                  <p class='link'>Click here to <a href='Login_Form.php'>Login</a></p>
-                  </div>";
-        } else {
-            echo "<div class='form'>
-                  <h3>Required fields are missing.</h3><br/>
-                  <p class='link'>Click here to <a href='Registration.php'>registration</a> again.</p>
-                  </div>";
-        }
-    } else {
-?>
-
-    <form class="form" action="" method="post" method="post">
-        <img src="blood.png" alt="" style="margin-top: -70px; margin-left: -50px">
-        <h1 class="login-title">Registration</h1>
-        <input type="text" class="login-input" name="fullname" placeholder="Full Name" required />
-        <input type="text" class="login-input" name="username" placeholder="Username" required />
-        <input type="text" class="login-input" name="email" placeholder="Email Adress">
-        <input type="password" class="login-input" name="password" placeholder="Password">
-        <input type="submit" name="submit" value="Register" class="login-button">
-        <p class="link"><a href="Login_Form.php">Click to Login</a></p>
-    </form>
-<?php
-    }
-?>
 </body>
 </html>
